d673092e3cfd618b12f96296283186bf69149230
shin-carpediem <buru.aoshin@gmail.com>
2022-11-02 07:05:30 +0900

    complete textview

--- SwiftUI100/MoreTextView.swift
+++ SwiftUI100/MoreTextView.swift
@@ -6,6 +6,28 @@ struct MoreTextView: View {
     @State private var isTextFold = false
     @State private var isShownFoldButton = true
     
+    // なぜこのプロパティがあればうまくいく？
+    @State private var isFirst = true
+    
+    private func updateTextSize(_ textSize: CGSize) {
+//        if !self.isTextFold {
+        if self.isFirst {
+            if textSize.height > 80 {
+                self.textHeight = 80
+                self.isTextFold = true
+                self.isShownFoldButton = true
+                
+                self.isFirst = false
+            } else {
+                self.textHeight = nil
+                self.isTextFold = false
+                self.isShownFoldButton = false
+                
+                self.isFirst = true
+            }
+        }
+    }
+    
     var body: some View {
         VStack(alignment: .trailing) {
             HStack {
@@ -16,14 +38,7 @@ struct MoreTextView: View {
                     })
                     .padding()
                     .onPreferenceChange(SizePreference.self) { textSize in
-                        if !self.isTextFold {
-                            if textSize.height > 80 {
-                                self.textHeight = 80
-                                self.isTextFold = true
-                            } else {
-                                self.isShownFoldButton = false
-                            }
-                        }
+                        updateTextSize(textSize)
                     }
                 Spacer()
             }
